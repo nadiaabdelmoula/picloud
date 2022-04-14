426b8693a187543db35065ea5cbf1c1ef47a63a0
nadiaabdelmoula <nadia.abdelmoula@esprit.tn>
2022-04-15 00:39:01 +0100

    template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { CreateReclamationComponent } from './reclamation/create-reclamation/create-reclamation.component';
+import { UpdateReclamationComponent } from './reclamation/update-reclamation/update-reclamation.component';
+import { AdminReclamationComponent } from './reclamation/admin-reclamation/admin-reclamation.component';
+import { ListReclamationComponent } from './reclamation/list-reclamation/list-reclamation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent,
+    CreateReclamationComponent,
+    UpdateReclamationComponent,
+    AdminReclamationComponent,
+    ListReclamationComponent
   ],
   imports: [
     BrowserModule,
